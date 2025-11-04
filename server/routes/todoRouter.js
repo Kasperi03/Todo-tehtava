cd4e86e5482c0f0984083385d19c0bf5a7b0d5b7
Kasperi03 <kasperiveeti03@gmail.com>
2025-11-04 16:02:05 +0200

    todo 5 tehty

---
+++ server/routes/todoRouter.js
@@ -0,0 +1,32 @@
+import { pool } from '../helper/db.js'
+import { Router } from 'express'
+
+const router = Router()
+
+router.get('/', (req, res, next) => {
+ pool.query('SELECT * FROM task', (err, result) => {
+ if (err) {
+ return next (err)
+ }
+ res.status(200).json(result.rows || [])
+ })
+})
+
+router.delete('/delete/:id', (req, res,next) => {
+ const { id } = req.params
+ pool.query('delete from task WHERE id = $1',
+ [id],
+ (err, result) => {
+ if (err) {
+ return next(err)
+ }
+ if (result.rowCount === 0) {
+ const error = new Error('Task not found')
+ error.status = 404
+ return next(error)
+ }
+ return res.status(200).json({id:id})
+ })
+})
+
+export default router
